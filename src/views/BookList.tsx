1d1aa54e6dd18a982ce81ff1721898f38234f999
jskim <ks78mk2@gmail.com>
2023-05-20 20:07:08 +0900

    북마크 기능 추가

--- src/views/BookList.tsx
+++ src/views/BookList.tsx
@@ -1,6 +1,8 @@
 import { Navigation } from 'swiper';
 import { useState, useEffect } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
+import Icon from '@mdi/react';
+import { mdiBookmark } from '@mdi/js';
 import 'swiper/css';
 import "swiper/css/navigation";
 import BookService from 'services/books'
@@ -9,24 +11,35 @@ const bookImage = require('../assets/images/book.png');
 interface Book {
   id : string,
   title : string,
-  description ?: string
+  description : string,
+  bookmark ?: boolean
 }
 
-const slideContent = (props : Book) =>  {
+const SlideContent = ({item, bookmarkFunction} : any) =>  {
   return (
     <>
       <div style={{ backgroundImage : `url(${bookImage})`, width: '230px', height: '260px'}}>
-        {props.title}
-        {props.description}
+        <div onClick={ () => {bookmarkFunction(item.id)} }>
+          <Icon 
+            className='bookmark' 
+            color={`${item.bookmark ? '#FF893C' : '#B2B2B2'}`} 
+            path={mdiBookmark}
+          />
+        </div>
+        <div className='bookTitle'>{item.title}</div>
+        <div className='bookDesc'>{item.description}</div>
       </div>
     </>
   )
 }
 
 const BookList = () => {  
-  const [books, setBooks] = useState([]);
+  const [books, setBooks] = useState([] as any);
   
   useEffect(() => {
+    if (localStorage.getItem('bookmark') == null) {
+      localStorage.setItem('bookmark', JSON.stringify([]))
+    }
     getBooks();
   },[]);
   
@@ -34,12 +47,47 @@ const BookList = () => {
     let response : any = await BookService.getBookList();
 
     const data = response.data;
+    let newData = [...data]
+    for (let i=0; i<newData.length; i++) {
+      if (localStorage.getItem('bookmark') !== null) {
+        let _bookmark: string | null = localStorage.getItem('bookmark');
+        let bookmarkArr = [];
+        if (_bookmark != null) {
+          bookmarkArr = JSON.parse(_bookmark);	
+        }
+
+        if (bookmarkArr.indexOf(newData[i].id) > -1) {
+          newData[i].bookmark = true;
+        } else {
+          newData[i].bookmark = false;
+        }
+      } else {
+        newData[i].bookmark = false;
+      }
+    }
     setBooks(data);
   }
 
-  const slides = Array.from({ length: 10 }).map(
-    (el, index) => `Slide ${index + 1}`
-  );
+  const handleBookmark = (id : string) => {
+    let _bookmarkStr : string = localStorage.getItem('bookmark') || JSON.stringify([]);
+    let bookmarkArr = JSON.parse(_bookmarkStr);
+    if (bookmarkArr.indexOf(id) > -1) {
+      bookmarkArr.splice(bookmarkArr.indexOf(id), 1);
+    } else {
+      bookmarkArr.push(id);
+    }
+    console.log('bookmarkArr: ', bookmarkArr);
+    
+    localStorage.setItem('bookmark', JSON.stringify(bookmarkArr));
+    
+    let updateBooks : Array<any> = [...books];
+    for (let i=0; i<updateBooks.length; i++) {
+      if (updateBooks[i].id == id) {
+        updateBooks[i].bookmark = !updateBooks[i].bookmark;
+      }
+    }
+    setBooks(updateBooks);
+  }
 
   return (
     <div className="contentWrapper">
@@ -53,9 +101,9 @@ const BookList = () => {
           modules={[Navigation]} 
           slidesPerView={'auto'}
         >
-          {books.map((item : Book, index) => (
+          {books.map((item : Book, index: number) => (
             <SwiperSlide key={item.id}>
-              {slideContent(item)}
+              <SlideContent item={item} bookmarkFunction={handleBookmark}></SlideContent>
             </SwiperSlide>
           ))}
       </Swiper>
